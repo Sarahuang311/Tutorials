cfb49f26101090a95096a298bafa29f0eee9ff2a
Shengchun Huang <huang.shengchun311@gmail.com>
2026-01-14 08:23:13 -0600

    W1 handout

---
+++ W1/J381M_H01_R_basics.R
@@ -0,0 +1,15 @@
+# Project: j381m week 1 tutorial on R_basics
+# Date: 2026-01-15
+# Author: Your Name
+# Description: This script demonstrates basic R operations
+
+
+setwd("~/Library/CloudStorage/Box-Box/UT/Teaching/J381M_Spring26/Tutorials/W1/new_folder") 
+read.csv("tips.txt", stringsAsFactors = T)
+
+
+# 2. R basics ------------
+## R as a calculator -------------
+## Assign values to variables -------------
+
+### Functions and arguments ------------
